9a06b2506ec439b0727ee56afd43c611204b1e08
Bruno Viana <brunolviana22@hotmail.com>
2019-08-22 01:34:40 -0300

    Ajuste de import
    Apenas um ajuste de import, pois o marcador ValidaEndereco foi movido de
    pacote.

--- src/main/java/br/com/valhala/academia/validacao/ValidadorEndereco.java
+++ src/main/java/br/com/valhala/academia/validacao/ValidadorEndereco.java
@@ -12,6 +12,8 @@ import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
 
+import br.com.valhala.academia.validacao.marcadores.ValidaEndereco;
+
 @Named
 @ValidaEndereco
 public class ValidadorEndereco implements Validador, Serializable {
