d99a5b50d36179e17edec11bfd605892cd42a835
Bruno Viana <brunolviana22@hotmail.com>
2019-08-29 00:29:02 -0300

    Gravação de Foto Aluno
    Ajuste de validação para utilizar Regex.

--- src/main/java/br/com/valhala/academia/web/validators/FileInputImageValidator.java
+++ src/main/java/br/com/valhala/academia/web/validators/FileInputImageValidator.java
@@ -11,18 +11,31 @@ import javax.faces.validator.ValidatorException;
 import javax.inject.Inject;
 import javax.servlet.http.Part;
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @FacesValidator("fileInputImageValidator")
 public class FileInputImageValidator implements Validator {
 
+    Pattern pattern;
+    Matcher matcher;
+
     @Override
     public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+
         Part file = (Part) value;
-        if (!StringUtils.equalsAny(file.getContentType(), "image/gif", "image/jpeg", "image/png")) {
-            FacesMessage message = new FacesMessage("Formato de arquivo inválido. Deve ser um arquivo .gif, .jpeg ou .png.");
-            message.setSeverity(FacesMessage.SEVERITY_WARN);
-            throw new ValidatorException(message);
+
+        if (file != null) {
+            pattern = Pattern.compile("^(image)\\/{1}(gif|jpe?g|png)$");
+            matcher = pattern.matcher(file.getContentType());
+
+            if (!matcher.matches()) {
+                FacesMessage message = new FacesMessage("Formato de arquivo inválido. Deve ser um arquivo .gif, .jpeg ou .png.");
+                message.setSeverity(FacesMessage.SEVERITY_WARN);
+                throw new ValidatorException(message);
+            }
         }
+
     }
 
 }
