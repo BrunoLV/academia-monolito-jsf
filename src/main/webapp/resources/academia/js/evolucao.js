dd4cdf0f3e8a7edd161a1fe28e15f4b285585a46
Bruno Viana <brunolviana22@hotmail.com>
2019-10-02 07:53:23 -0300

    Ajuste de Formatação
    Ajustei a formatação do arquivo

--- src/main/webapp/resources/academia/js/evolucao.js
+++ src/main/webapp/resources/academia/js/evolucao.js
@@ -20,31 +20,44 @@ var evolucao = (function() {
 					console.log(data);
 					medidas = data;
 
-					dadosGrafico.push([ 'Data Medição', 'Peso', 'Pescoço',
-							'Tórax Superior', 'Tórax Inferior', 'Busto',
-							'Cintura', 'Abdômen', 'Quadril', 'Coxa Direita',
-							'Coxa Esquerda', 'Panturrilha Direita',
-							'Panturrilha Esquerda', 'Braço Direito',
-							'Braço Esquerdo', 'Antebraço Direito',
-							'Antebraço Esquerdo' ]);
+					dadosGrafico.push([ 'Data Medição'
+									  , 'Peso'
+									  , 'Pescoço'
+									  , 'Tórax Superior'
+									  , 'Tórax Inferior'
+									  , 'Busto'
+									  , 'Cintura'
+									  , 'Abdômen'
+									  , 'Quadril'
+									  , 'Coxa Direita'
+									  , 'Coxa Esquerda'
+									  , 'Panturrilha Direita'
+									  , 'Panturrilha Esquerda'
+									  , 'Braço Direito'
+									  , 'Braço Esquerdo'
+									  , 'Antebraço Direito'
+									  , 'Antebraço Esquerdo' ]);
 
 					var i;
 					for (i = 0; i < medidas.length; i++) {
-						dadosGrafico.push([ medidas[i].dataMedicao,
-								medidas[i].peso, medidas[i].pescoco,
-								medidas[i].toraxSuperior,
-								medidas[i].toraxInferior, medidas[i].busto,
-								medidas[i].cintura, medidas[i].abdomen,
-								medidas[i].quadril, medidas[i].coxaDireita,
-								medidas[i].coxaEsquerda,
-								medidas[i].panturrilhaDireita,
-								medidas[i].panturrilhaEsquerda,
-								medidas[i].bracoDireito,
-								medidas[i].bracoEsquerdo,
-								medidas[i].antebracoDireito,
-								medidas[i].antebracoEsquerdo ]);
-					}
-					;
+						dadosGrafico.push([ medidas[i].dataMedicao
+							, medidas[i].peso
+							, medidas[i].pescoco
+							, medidas[i].toraxSuperior
+							, medidas[i].toraxInferior
+							, medidas[i].busto
+							, medidas[i].cintura
+							, medidas[i].abdomen
+							, medidas[i].quadril
+							, medidas[i].coxaDireita
+							, medidas[i].coxaEsquerda
+							, medidas[i].panturrilhaDireita
+							, medidas[i].panturrilhaEsquerda
+							, medidas[i].bracoDireito
+							, medidas[i].bracoEsquerdo
+							, medidas[i].antebracoDireito
+							, medidas[i].antebracoEsquerdo ]);
+					};
 
 					var data = google.visualization
 							.arrayToDataTable(dadosGrafico);
